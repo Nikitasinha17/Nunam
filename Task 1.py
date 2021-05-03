148e1ad82c24ddb073e55b76ddcf7b5b05c25353
Nikitasinha17 <80613108+Nikitasinha17@users.noreply.github.com>
2021-05-03 23:56:23 +0530

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -1,57 +1,36 @@
 import pandas as pd
-#defining sheets 
-sheets = []
-x = pd.ExcelFile('data.xs')
-for sn in x.sheet_names:
-    if 'Detail_67_' in sn:
-        sheets.append(sn)
-df = []
-#looping the list of sheets 
-for i in sheets:
-    df.append(pd.read_excel('data.xs', sheet_name = i))
-#concatinate the data list into dataframe
-df_final = pd.concat(df)
-#write the dataframe to the csv file
-df_final.to_csv('detail.csv')
-print("Successfully created.")
+
+sheets = pd.read_excel("data.xlsx", sheet_name=None)
+sheets2 = pd.read_excel("data_1.xlsx", sheet_name=None)
 
 
-sheets = []
-x = pd.ExcelFile('data.xls')
-for sn in x.sheet_names:
-    if 'DetailVol_67_' in sn:
-        sheets.append(sn)
-df = []
-#looping through the list of sheets 
-for i in sheets:
-    df.append(pd.read_excel('data.xs', sheet_name = i))
-#concating the data list into dataframe
-df_final = pd.concat(df)
-#writing the dataframe to the csv file
-df_final.to_csv('detailVol.csv')
-print("Successfully created.")
+detail = pd.DataFrame()
+detailVol = pd.DataFrame()
+detailTemp = pd.DataFrame()
 
 
-sheet1 = []
-x1 = pd.ExcelFile('data.xs')
-for sn in x1.sheet_names:
-    if 'DetailTemp_67_' in sn:
-        sheet1.append(sn)
+for i in sheets.keys():
+    if('Detail_67_' in i):
+        temp = sheets[i]
+        detail = pd.concat([details, df])
+    elif('DetailVol_67_' in i):
+        temp2 = sheets[i]
+        detailVol = pd.concat([detailVol, df2])
+    elif('DetailTemp_67_' in i):
+        temp3 = sheets[i]
+        detailTemp = pd.concat([detailTemp, df3])
 
-sheet2 = []
-x2 = pd.ExcelFile('data_1.xs')
-for sn in x2.sheet_names:
-    if 'DetailTemp_67_' in sn:
-        sheet2.append(sn)
-df = []
-#looping through the list of sheets
-for i in sheet1:
-    df.append(pd.read_excel('data.xs', sheet_name = i))
-for i in sheet2:
-    df.append(pd.read_excel('data_1.xs', sheet_name = i))
-    
-#concating the data list into dataframe
-df_final = pd.concat(df)
-#writing the dataframe to the csv file
-df_final.to_csv('detailTemp.csv')
-print("Successfully created.")
+for i in sheets2.keys():
+    if('Detail_67_' in i):
+        temp = sheets2[i]
+        detail = pd.concat([details, df])
+    elif('DetailVol_67_' in i):
+        temp2 = sheets2[i]
+        detailVol = pd.concat([detailVol, df2])
+    elif('DetailTemp_67_' in i):
+        temp3 = sheets2[i]
+        detailTemp = pd.concat([detailTemp, df3])
+
+detail.to_csv('detail.csv')
+detailVol.to_csv('detailVol.csv')
+detailTemp.to_csv('detailTemp.csv')
